512fc6531000fca543145e872e4e034b3084f27d
Supraja Gorre <M328633@one.merckgroup.com>
2025-04-24 13:12:14 +0530

    added new code

--- shiny_code1.R
+++ shiny_code1.R
@@ -1,7 +1,7 @@
 library(shiny)
 library(plotly)
 
-# ui <- navbarPage(
+# ui <- fluidPage(navbarPage(
 #   "My App",
 #   tabPanel("Tab 1",
 #            # Content for Tab 1
@@ -9,6 +9,7 @@ library(plotly)
 #   tabPanel("Tab 2",
 #            # Content for Tab 2
 #   )
+# )
 # )
 
 # ui <- fluidPage(
@@ -47,19 +48,36 @@ library(plotly)
 # server <- function(input, output){
 # }
 
+# 
+# ui <-     fluidPage(
+#   sliderInput("val","select line value",
+#               0.1,5,value=1),
+#   plotlyOutput("plotly"),
+# )
+# 
+# # Server
+# server <- function(input, output) {
+#   output$plotly <- renderPlotly({
+#     plot_ly(data = iris, x = ~Sepal.Length, y = ~Sepal.Width, color = ~Species, type = "scatter", mode = "markers") %>%
+#     add_lines(y = ~input$val)
+#   })
+# }
 
-ui <-     fluidPage(
-  sliderInput("val","select line value",
-              0.1,5,value=1),
-  plotlyOutput("plotly"),
-)
+# ui <- fluidPage(
+#   sliderInput("obs", "Number of observations", 0, 1000, 500),
+#   actionButton("goButton", "Go!", class = "btn-success"),
+#   plotOutput("distPlot")
+# )
+# server <- function(input, output) {
+#   output$distPlot <- renderPlot({
+#     # Take a dependency on input$goButton. This will run once initially,
+#     # because the value changes from NULL to 0.
+#     input$goButton
+#     # Use isolate() to avoid dependency on input$obs
+#     dist <- isolate(rnorm(input$obs))
+#     hist(dist)
+#   })
+# }
 
-# Server
-server <- function(input, output) {
-  output$plotly <- renderPlotly({
-    plot_ly(data = iris, x = ~Sepal.Length, y = ~Sepal.Width, color = ~Species, type = "scatter", mode = "markers") %>%
-    add_lines(y = ~input$val)
-  })
-}
 
 shinyApp(ui, server)
