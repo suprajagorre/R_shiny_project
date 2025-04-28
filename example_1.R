5aa4b11a97fe1c3d73111bb87b235966a333face
Supraja Gorre <M328633@one.merckgroup.com>
2025-04-28 17:25:41 +0530

    new file

---
+++ example_1.R
@@ -0,0 +1,41 @@
+library(shiny)
+
+# ui <- fluidPage(
+#   fileInput("upload", NULL, accept = c(".csv", ".tsv")),
+#   numericInput("n", "Rows", value = 5, min = 1, step = 1),
+#   tableOutput("head")
+# )
+# 
+# server <- function(input, output, session) {
+#   data <- reactive({
+#     req(input$upload)
+#     
+#     ext <- tools::file_ext(input$upload$name)
+#     switch(ext,
+#            csv = vroom::vroom(input$upload$datapath, delim = ","),
+#            tsv = vroom::vroom(input$upload$datapath, delim = "\t"),
+#            validate("Invalid file; Please upload a .csv or .tsv file")
+#     )
+#   })
+#   
+#   output$head <- renderTable({
+#     head(data(), input$n)
+#   })
+# }
+
+ui <- fluidPage(
+  fileInput("upload", "Upload csv file", accept = ".csv"),
+  tableOutput("head")
+)
+
+server <- function(input, output, session) {
+  data <- reactive({
+    req(input$upload)
+    csv = vroom::vroom(input$upload$datapath, delim = ",")
+  })
+  
+  output$head <- renderTable({
+    head(data(), 10)
+  })
+}
+shinyApp(ui, server)
