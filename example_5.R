f12259d9a75a738df4d4821f903175241240ea66
Supraja Gorre <M328633@one.merckgroup.com>
2025-05-09 15:07:15 +0530

    added

--- example_5.R
+++ example_5.R
@@ -3,4 +3,58 @@
 
 
 
+library(shiny)
+library(shinythemes)
+library(ggplot2)
+
+ui <- fluidPage(theme= shinytheme("united"),
+                navbarPage(
+                  "My Shiny App",
+                  tabPanel("tab 1",
+                           sidebarPanel(
+                             fileInput("upload", "Upload csv file", accept = ".csv")
+                           ),
+                           mainPanel(
+                             tableOutput("head")
+                           )
+                  ),
+                  tabPanel("tab 2",
+                           sidebarPanel(
+                             selectInput("x", "X", choices = NULL),
+                             selectInput("y", "Y", choices = NULL),
+                             selectInput("color", "color", choices = NULL),
+                             actionButton("simulate", "plot",class = "btn-success")
+                           ),
+                           mainPanel(
+                             plotOutput("plot")
+                           )
+                  )
+                ))
+
+server <- function(input, output, session) {
+  data <- reactive({
+    req(input$upload)
+    read.csv(input$upload$datapath)
+  })
+  
+  output$head <- renderTable({
+    head(data(), 10)
+  })
+  
+  observeEvent(data(), {
+    choices <- names(data())
+    updateSelectInput(inputId = "x", choices = choices)
+    updateSelectInput(inputId = "y", choices = choices)
+    updateSelectInput(inputId = "color", choices = choices)
+  })
+  
+  
+  output$plot <- renderPlot({
+    if(input$simulate>0){
+      ggplot(data=data(), 
+             aes(x=get(input$x), y=get(input$y), colour= get(input$color))) + 
+        geom_point(alpha=0.1) +
+        geom_smooth()
+    }})
+}
 shinyApp(ui, server)
