278f486c776aacc150dc269f77b913091f931133
Supraja Gorre <M328633@one.merckgroup.com>
2025-05-15 23:15:13 +0530

    added cide

--- example_1.R
+++ example_1.R
@@ -8,6 +8,7 @@ ui <- fluidPage(theme= shinytheme("united"),
     tabPanel("tab 1",
       sidebarPanel(
         fileInput("upload", "Upload csv file", accept = ".csv")
+        # numericInput("numInput", "Enter a number:", value = 0)
       ),
       mainPanel(
         tableOutput("head")
@@ -23,8 +24,13 @@ server <- function(input, output, session) {
   })
   
   output$head <- renderTable({
+    # if(input$numInput<5){validate("
+    #   enter number more than 5"
+    # )}
+    # head(data(), input$numInput)
     head(data(), 10)
   })
+
   
   # output$head <- renderDT(data(), options = list(pageLength = 5))
   # output$head <- renderDT(data(), options = list(pageLength = 5))
