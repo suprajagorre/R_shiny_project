ac396efcafe088e98fec7baff4abccf8bf01d65a
Supraja Gorre <M328633@one.merckgroup.com>
2025-07-21 18:13:45 +0530

    added code

--- worksho_2_hands_on.R
+++ worksho_2_hands_on.R
@@ -5,6 +5,7 @@ library(tidyverse)
 library(rlistings)
 library(shinythemes)
 library(DT)
+library(plotly)
 
 ui <- navbarPage(theme= shinytheme("united"),"MS123 Analysis",
                  tabPanel("Data",
@@ -13,6 +14,26 @@ ui <- navbarPage(theme= shinytheme("united"),"MS123 Analysis",
                               tabPanel("Subjects data",
                                        mainPanel(
                                          dataTableOutput("sl")
+                                       )),
+                            tabPanel("Adverse Events",
+                                     mainPanel(
+                                       dataTableOutput("ae")
+                                     ))
+                          )
+                          )
+                 ),
+                 tabPanel("Table",
+                          mainPanel(
+                            tabsetPanel(
+                              tabPanel("Lab table",
+                                       mainPanel(
+                                         verbatimTextOutput("Table1"),
+                                         width = 12
+                                       )),
+                              tabPanel("Plot",
+                                       mainPanel(
+                                         plotlyOutput("plot")
+                                         # width = 12
                                        ))
                             )
                           )
@@ -21,8 +42,38 @@ ui <- navbarPage(theme= shinytheme("united"),"MS123 Analysis",
 
 server <- function(input, output, session) {
 
-  output$sl<- renderDT(ex_adsl, options = list(pageLength = 5))
-  
+   output$sl<- renderDT(ex_adsl, options = list(pageLength = 10))
+   output$ae<- renderDT(ex_adae, options = list(pageLength = 10))
+   
+   data <- reactive({ex_adlb %>%
+       group_by(USUBJID,PARAMCD) %>%
+       arrange(USUBJID,PARAMCD, AVAL)%>%
+       mutate(
+         #first.
+         MIN = if_else(row_number(AVAL) == 1, "Y", ""),
+         #last.
+         MAX = if_else(row_number(AVAL) == n(), "Y", "")
+       )%>% filter(SUBJID =="id-105")%>% select(USUBJID,PARAMCD,AVAL,AVISIT, MIN, MAX)})
+  output$Table1 <- renderPrint({
+    lsting <- as_listing(
+      df = data(),
+      disp_cols = c( "PARAMCD","AVAL", "MIN", "MAX"),
+      key_cols = c("USUBJID", "AVISIT"),
+      main_title = "Lab listing",
+      subtitles = c("Other sub titles1", "Other sub titles2"),
+      main_footer = c("Footnote1", "Footnote2"),
+      prov_footer = "Source:ADLB, data:"
+    )
+    lsting
+  })
+  output$plot <- renderPlotly({
+    plot_ly(data = ex_adlb, x = "ADY", y = "AVAL", type = "scatter", mode = "lines")%>%
+      layout(
+        title = "Customized Scatter Plot",
+        xaxis = list(title = ""),
+        yaxis = list(title = "")
+      )
+  })
 }
 
 
