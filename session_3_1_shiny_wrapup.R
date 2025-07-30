3d7913508cc30233ab913eb074d6b79625f86b1b
Supraja Gorre <M328633@one.merckgroup.com>
2025-07-30 09:41:08 +0530

    added comments

--- session_3_1_shiny_wrapup.R
+++ session_3_1_shiny_wrapup.R
@@ -1,75 +1,95 @@
+# Load necessary libraries for the Shiny app and data manipulation
 library(shiny)
-library(formatters)
-library(DT)
-library(shinythemes)
-library(dplyr)
-library(plotly)
+library(formatters)  # For formatting data
+library(DT)          # For rendering DataTables
+library(shinythemes) # For applying themes to the UI
+library(dplyr)       # For data manipulation
+library(plotly)      # For interactive plots
 
-ui <- navbarPage(theme= shinytheme("united"), "MSx123",
-                 tabPanel("Data",                           sidebarPanel(
-                   selectInput("usubjid", "USUBJID", choices = NULL),
-                   # selectInput("arm", "ARM", choices = NULL),
-                   # selectInput("sex", "SEX", choices = NULL),
-                   actionButton("simulate", "Submit",class = "btn-success")
-                 ),
+# Define the user interface (UI) of the Shiny app
+ui <- navbarPage(theme = shinytheme("united"), "MSx123",
+                 tabPanel("Data",                           # Create a tab for data
+                          sidebarPanel(
+                            selectInput("usubjid", "USUBJID", choices = NULL),  # Dropdown for USUBJID
+                            # Uncomment the following lines to add more dropdowns for ARM and SEX
+                            # selectInput("arm", "ARM", choices = NULL),
+                            # selectInput("sex", "SEX", choices = NULL),
+                            actionButton("simulate", "Submit", class = "btn-success")  # Submit button
+                          ),
                           mainPanel(
-                            tabsetPanel(
+                            tabsetPanel(  # Create a set of tabs in the main panel
                               tabPanel("Subjects data",
                                        mainPanel(
-                                         dataTableOutput("adsl")
+                                         dataTableOutput("adsl")  # Output table for subjects data
                                        )),
                               tabPanel("Adverse Events",
                                        mainPanel(
-                                         dataTableOutput("adae")
+                                         dataTableOutput("adae")  # Output table for adverse events
                                        )),
                               tabPanel("Lab Plot",
                                        mainPanel(
-                                         plotlyOutput("plot")
+                                         plotlyOutput("plot")  # Output for interactive lab plot
                                        ))
                             )
                           )
                  ))
-  
+
+# Define the server logic for the Shiny app
 server <- function(input, output, session) {
-observeEvent(ex_adsl, {
-  updateSelectInput(inputId = "usubjid", choices = ex_adsl$USUBJID)
-  # updateSelectInput(inputId = "arm", choices = unique(ex_adsl$ARMCD))
-  # updateSelectInput(inputId = "sex", choices = unique(ex_adsl$SEX))
-})
   
-output$adsl <- renderDT({
-  if(input$simulate>0){
-    adsl <- ex_adsl %>% filter(USUBJID==input$usubjid)
-    adsl
-    }else{ex_adsl}})
-
-output$adae <- renderDT({
-  if(input$simulate>0){
-    adae <- ex_adae %>% filter(USUBJID==input$usubjid)
-    adae
-  }else{ex_adae}})
-
-output$plot <- renderPlotly({
-  if(input$simulate>0){
-    adlb<- ex_adlb %>% filter(USUBJID==input$usubjid & PARAMCD=="ALT")
-    plot_ly(data = adlb, x = ~ADY, y = ~AVAL, color = ~USUBJID, type = "scatter", mode = "lines")%>% add_lines() %>%
-      layout(
-        title = "Lab Plot",
-        xaxis = list(title = "Time since treatment"),
-        yaxis = list(title = " ALT "),
-        height = 800, width = 1200
-      )
-  }else{    adlb<- ex_adlb %>% filter(PARAMCD=="ALT")
-  plot_ly(data = adlb, x = ~ADY, y = ~AVAL, color = ~USUBJID, type = "scatter", mode = "lines")%>% add_lines() %>%
-    layout(
-      title = "Lab Plot",
-      xaxis = list(title = "Time since treatment"),
-      yaxis = list(title = " ALT "),
-      height = 800, width = 1200
-    )
-}})
-
-
+  # Observe changes in ex_adsl and update the USUBJID dropdown choices
+  observeEvent(ex_adsl, {
+    updateSelectInput(inputId = "usubjid", choices = ex_adsl$USUBJID)  # Update choices for USUBJID
+    # Uncomment the following lines to update choices for ARM and SEX
+    # updateSelectInput(inputId = "arm", choices = unique(ex_adsl$ARMCD))
+    # updateSelectInput(inputId = "sex", choices = unique(ex_adsl$SEX))
+  })
+  
+  # Render the subjects data table based on selected USUBJID
+  output$adsl <- renderDT({
+    if (input$simulate > 0) {  # Check if the submit button has been pressed
+      adsl <- ex_adsl %>% filter(USUBJID == input$usubjid)  # Filter data for selected USUBJID
+      adsl  # Return the filtered data
+    } else {
+      ex_adsl  # Return the full dataset if not simulated
+    }
+  })
+  
+  # Render the adverse events data table based on selected USUBJID
+  output$adae <- renderDT({
+    if (input$simulate > 0) {  # Check if the submit button has been pressed
+      adae <- ex_adae %>% filter(USUBJID == input$usubjid)  # Filter data for selected USUBJID
+      adae  # Return the filtered data
+    } else {
+      ex_adae  # Return the full dataset if not simulated
+    }
+  })
+  
+  # Render the lab plot based on selected USUBJID
+  output$plot <- renderPlotly({
+    if (input$simulate > 0) {  # Check if the submit button has been pressed
+      adlb <- ex_adlb %>% filter(USUBJID == input$usubjid & PARAMCD == "ALT")  # Filter for ALT parameter
+      plot_ly(data = adlb, x = ~ADY, y = ~AVAL, color = ~USUBJID, type = "scatter", mode = "lines") %>%
+        add_lines() %>%
+        layout(
+          title = "Lab Plot",  # Title of the plot
+          xaxis = list(title = "Time since treatment"),  # X-axis label
+          yaxis = list(title = "ALT"),  # Y-axis label
+          height = 800, width = 1200  # Dimensions of the plot
+        )
+    } else {
+      adlb <- ex_adlb %>% filter(PARAMCD == "ALT")  # Filter all data for ALT parameter
+      plot_ly(data = adlb, x = ~ADY, y = ~AVAL, color = ~USUBJID, type = "scatter", mode = "lines") %>%
+        add_lines() %>%
+        layout(
+          title = "Lab Plot",  # Title of the plot
+          xaxis = list(title = "Time since treatment"),  # X-axis label
+          yaxis = list(title = "ALT"),  # Y-axis label
+          height = 800, width = 1200  # Dimensions of the plot
+        )
+    }
+  })
 }
 
+# Run the Shiny app
 shinyApp(ui, server)
--- session_3_2_shiny_wrapup.R
+++ session_3_2_shiny_wrapup.R
@@ -1,76 +1,84 @@
+# Load necessary libraries for the Shiny app and DataTable functionality
 library(shiny)
 library(DT)
 
-
+# Define the user interface (UI) of the Shiny app
 ui = fluidPage(
   fluidRow(
-    column(2, selectInput('dataset','Select dataset', choices = c("ADSL","ADAE","ADLB"),
-                          selected='',
-                          multiple=TRUE)),
-    column(4, textInput('comment',"Add comment",value = "", width = NULL,placeholder = "comment")),
-    column(2, actionButton("button","Submit")),
-    # Button
-    column(7, downloadButton("downloadData", "Download"))
+    # Column for selecting datasets
+    column(2, selectInput('dataset', 'Select dataset', choices = c("ADSL", "ADAE", "ADLB"),
+                          selected = '',  # Default selected value
+                          multiple = TRUE)),  # Allow multiple selections
+    # Column for adding comments
+    column(4, textInput('comment', "Add comment", value = "", width = NULL, placeholder = "comment")),
+    # Column for the submit button
+    column(2, actionButton("button", "Submit")),  # Button to submit comments
+    # Column for the download button
+    column(7, downloadButton("downloadData", "Download"))  # Button to download data
   ),
-  fluidRow (
-    column(12, dataTableOutput('data') ) 
+  fluidRow(
+    # Column for displaying the data table
+    column(12, dataTableOutput('data'))  # Output area for the data table
   )           
 )
 
+# Define the server logic for the Shiny app
 server <- function(input, output, session) {
-
+  
+  # Reactive value to store comments
   df_comments <- reactiveVal({
     data.frame(
-      dataset = character(0), 
-      Comment = character(0)
+      dataset = character(0),  # Placeholder for dataset names
+      Comment = character(0)    # Placeholder for comments
     )
   })
-  dat = data.frame(dataset="ADSL",Comment="example comment") 
-  ## reactive object df
+  
+  # Initial data frame with an example comment
+  dat = data.frame(dataset = "ADSL", Comment = "example comment") 
+  
+  # Reactive object to manage the current data frame
   df_current <- reactive({
-    
-    ## reactivity that df depends on
-    ## currently df = dat does not change
+    # Start with the initial dataset
     df <- dat
     
-    ## merge with current comments
-    if(nrow(df_comments()) > 0)
-      df <- rbind(df, df_comments())
+    # If there are comments, merge them with the initial dataset
+    if (nrow(df_comments()) > 0)
+      df <- rbind(df, df_comments())  # Combine the data frames
     
-    return(df)
-    
+    return(df)  # Return the updated data frame
   })
   
+  # Observe the submit button click event
   observeEvent(input$button, {
+    req(input$dataset)  # Ensure a dataset is selected
     
-    req(input$dataset)
-    
-    ## update df_comments by adding comments
+    # Update df_comments by adding the new comment
     df_comments_new <- rbind(df_comments(), 
                              data.frame(dataset = input$dataset, Comment = input$comment)
     )
-    df_comments(df_comments_new)
-    
+    df_comments(df_comments_new)  # Update the reactive value with the new comments
   })
   
+  # Render the data table output
   output$data <- renderDataTable({
-    if(input$button>0){
-    df_current()
-  }else{
-    data.frame(dataset="ADSL",Comment="example comment") 
-  }
+    if (input$button > 0) {  # If the submit button has been pressed
+      df_current()  # Return the updated data frame
+    } else {
+      # Return the initial dataset if no comments have been submitted
+      data.frame(dataset = "ADSL", Comment = "example comment") 
+    }
   })
   
-  # Downloadable csv of selected dataset ----
+  # Downloadable CSV of the current dataset
   output$downloadData <- downloadHandler(
     filename = function() {
-      paste("comments", ".csv", sep = "")
+      paste("comments", ".csv", sep = "")  # Define the filename for the download
     },
     content = function(file) {
-      write.csv(df_current(), file, row.names = FALSE)
+      write.csv(df_current(), file, row.names = FALSE)  # Write the current data frame to a CSV file
     }
   )
 }
 
-
+# Run the Shiny app
 shinyApp(ui, server)
